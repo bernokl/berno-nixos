6a7dc05e4fcc9d04dc2d6df2bf44bb541395f7dc
bernokl <bernokl@gmail.com>
2023-12-27 10:31:00 -0500

    Initial commit

---
+++ cli.nix
@@ -0,0 +1,133 @@
+{ config, pkgs, lib, inputs, ... }:
+{
+
+  environment.variables = {
+    EDITOR = "nvim";
+    #LC_ALL = "en_US.UTF-8";
+    TERM = "xterm-256color";
+  };
+
+  users.defaultUserShell = lib.getExe pkgs.zsh;
+
+  programs.zsh = {
+    enableBashCompletion = true;
+    autosuggestions.enable = true;
+    autosuggestions.extraConfig.ZSH_AUTOSUGGEST_USE_ASYNC = "y";
+    enable = true;
+    enableCompletion = true;
+    histFile = "$HOME/.cache/.zsh_history";
+    histSize = 100000;
+    syntaxHighlighting.enable = true;
+    syntaxHighlighting.highlighters = [ "main" "brackets" "pattern" "root" "line" ];
+    ohMyZsh.enable = true;
+    ohMyZsh.plugins = [ "sudo" "z" "aws" ];
+    shellInit = ''
+      #source ${pkgs.zsh-forgit}/share/zsh-forgit/forgit.plugin.zsh
+    '';
+    promptInit = let 
+	shell-config = pkgs.callPackage ./shell-config.nix {};
+    in ''
+      ${builtins.readFile (shell-config.override { dockerAliasEnabled = config.virtualisation.docker.enable; })}
+      autoload -U promptinit && promptinit && prompt pure
+      #complete -o nospace -C ${pkgs.terraform}/bin/terraform terraform
+      #complete -o nospace -C ${pkgs.awscli}/bin/aws_completer aws
+    '';
+
+    setOptions = [
+      "AUTO_CD"
+      "BANG_HIST"
+      "EXTENDED_HISTORY"
+      "HIST_EXPIRE_DUPS_FIRST"
+      "HIST_FIND_NO_DUPS"
+      "HIST_IGNORE_ALL_DUPS"
+      "HIST_IGNORE_DUPS"
+      "HIST_IGNORE_SPACE"
+      "HIST_REDUCE_BLANKS"
+      "HIST_SAVE_NO_DUPS"
+      "INC_APPEND_HISTORY"
+      "NOAUTOMENU"
+      "NOMENUCOMPLETE"
+      "SHARE_HISTORY"
+    ];
+
+  };
+
+  environment.shellAliases = {
+    ga = "git add";
+    gc = "git commit";
+    gcm = "git commit -m";
+    gs = "git status";
+    gsb = "git status -sb";
+
+    grep = "grep --color=auto";
+    diff = "diff --color=auto";
+    nixos-rebuild = "sudo nixos-rebuild";
+    auto-nix-rebuild = "ls /etc/nixos/**/*.nix | entr sudo bash -c 'nixos-rebuild switch && printf \"DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\"'";
+    opt = "manix '' | grep '^# ' | sed 's/^# \(.*\) (.*/\1/;s/ (.*//;s/^# //' | fzf --ansi --preview=\"manix '{}' | sed 's/type: /> type: /g' | bat -l Markdown --color=always --plain\"";
+
+    burn = "pkill -9";
+    external-ip = "dig +short myip.opendns.com @resolver1.opendns.com";
+    v = "$EDITOR $(fzf)";
+    sv = "sudo $EDITOR $(fzf)";
+    killall = "pkill";
+    q = "exit";
+    sc = "sudo systemctl";
+    scu = "systemctl --user ";
+    svi = "sudo $EDITOR";
+    net = "ip -c -br addr";
+
+    mkdir = "mkdir -p";
+    rm = "rm -Iv --preserve-root";
+    wget = "wget -c";
+
+    c = "xclip -selection clipboard";
+    cm = "xclip"; # Copy to middle click clipboard
+    l = "ls -lF --time-style=long-iso";
+    la = "l -a";
+    ls = "exa -h --git --color=auto --group-directories-first -s extension";
+    lstree = "ls --tree";
+    tree = "lstree";
+
+    ".." = "cd ..";
+    "..." = "cd ../../";
+    "...." = "cd ../../../";
+    "....." = "cd ../../../../";
+    "......" = "cd ../../../../../";
+  };
+
+  environment.systemPackages = with pkgs; [
+    bat
+    curl
+    entr
+    #eza
+    exa
+    fd
+    file
+    fzf
+    git
+    git-lfs
+    htop
+    inetutils
+    iotop
+    jq
+    lm_sensors
+    lshw
+    lsof
+    man
+    nettools
+    nix-top
+    nix-tree
+    nixpkgs-fmt
+    p7zip
+    parted
+    pciutils
+    psmisc
+    pure-prompt
+    ranger
+    ripgrep
+    unzip
+    wget
+    which
+    zip
+  ];
+}
---
+++ shell-config.nix
@@ -0,0 +1,106 @@
+{ lib
+, system
+, writeShellScript
+, runCommandNoCC
+, direnv
+, fd
+, gh
+, pure-prompt
+, sshuttle
+, dockerAliasEnabled ? true
+, prettyManPagesEnabled ? true
+, sshuttleEnabled ? true
+, zshPasteImprovementsEnabled ? true
+, exePathEnabled ? true
+}:
+
+
+with lib;
+let
+  prettyManPages = ''
+    export LESS_TERMCAP_mb=$(tput bold; tput setaf 2)
+    export LESS_TERMCAP_md=$(tput bold; tput setaf 3)
+    export LESS_TERMCAP_me=$(tput sgr0)
+    export LESS_TERMCAP_so=$(tput bold; tput setaf 3; tput setab 4)
+    export LESS_TERMCAP_se=$(tput rmso; tput sgr0)
+    export LESS_TERMCAP_us=$(tput smul; tput bold; tput setaf 2)
+    export LESS_TERMCAP_ue=$(tput rmul; tput sgr0)
+
+    export LESS_TERMCAP_mr=$(tput rev)
+    export LESS_TERMCAP_mh=$(tput dim)
+    export LESS_TERMCAP_ZN=$(tput ssubm)
+    export LESS_TERMCAP_ZV=$(tput rsubm)
+    export LESS_TERMCAP_ZO=$(tput ssupm)
+    export LESS_TERMCAP_ZW=$(tput rsupm)
+  '';
+  vpn = ''
+    function vpn() {
+        ${sshuttle}/bin/sshuttle --dns -r $1 0/0 --disable-ipv6 --no-latency-control
+    }
+  '';
+  exe-path = ''
+    function exe-path() {
+      readlink $(which $1)
+    }
+  '';
+  docker = ''
+    # Select a running docker container to stop
+    function docker-stop() {
+            local cid
+            cid=$(docker ps | sed 1d | fzf -q "$1" | awk '{print $1}')
+
+            [ -n "$cid" ] && docker stop "$cid"
+    }
+
+    # Select a docker container to remove
+    function docker-rm() {
+            local cid
+            cid=$(docker ps -a | sed 1d | fzf -q "$1" | awk '{print $1}')
+
+            [ -n "$cid" ] && docker rm "$cid"
+    }
+
+    # Select a container to attach onto with bash
+    function docker-bash() {
+            local cid
+            cid=$(docker ps -a | sed 1d | fzf -q "$1" | awk '{print $1}')
+
+            [ -n "$cid" ] && docker container exec -it "$cid" /bin/bash
+    }
+
+    docker-sh () {
+        local cid
+        cid=$(docker ps -a | sed 1d | fzf -q "$1" | awk '{print $1}')
+
+        [ -n "$cid" ] && docker container exec -it "$cid" /bin/sh
+    }
+  '';
+  zshPasteImprovements = ''
+    pasteinit() {
+      OLD_SELF_INSERT=''${''${(s.:.)widgets[self-insert]}[2,3]}
+      zle -N self-insert url-quote-magic
+    }
+
+    pastefinish() {
+      zle -N self-insert $OLD_SELF_INSERT
+    }
+
+    zstyle :bracketed-paste-magic paste-init pasteinit
+    zstyle :bracketed-paste-magic paste-finish pastefinish
+
+    bindkey -r ^V
+  '';
+in
+writeShellScript "shellconfig.sh" ''
+  if [ -n "''${commands[fzf-share]}" ]; then
+    source "$(fzf-share)/key-bindings.zsh"
+    source "$(fzf-share)/completion.zsh"
+  fi
+
+
+  ${optionalString dockerAliasEnabled docker}
+  ${optionalString prettyManPagesEnabled prettyManPages}
+  ${optionalString sshuttleEnabled vpn}
+  ${optionalString zshPasteImprovementsEnabled zshPasteImprovements}
+  ${optionalString exePathEnabled exe-path}
+''
